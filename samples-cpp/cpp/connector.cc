132af3cf7bb65b5c2cfa1378f2393074871884b0
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-11-06 15:04:00 +0800

    refine codes

---
+++ samples-cpp/cpp/connector.cc
@@ -0,0 +1,24 @@
+#include "../../dmerge-user-libs/include/syscall.h"
+#include "../include/allocator.hh"
+#include <gflags/gflags.h>
+#include <cassert>
+
+
+DEFINE_string(gid, "fe80:0000:0000:0000:248a:0703:009c:7ca0", "connect gid");
+
+DEFINE_int64(mac_id, 0, "machine id");
+DEFINE_int64(nic_id, 0, "nic idx. Should be align with gid");
+
+int
+main(int argc, char *argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    int sd = sopen();
+    assert(sd != 0);
+
+    int res = call_connect_session(sd, FLAGS_gid.c_str(), FLAGS_mac_id, FLAGS_nic_id);
+
+    std::cout << std::dec << "connect res:" << res << std::endl;
+    std::cout << "end" << std::endl;
+    return 0;
+}
