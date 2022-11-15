0360d6c5855bd9a863014c4bbd146126b5ecab82
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-11-15 19:15:45 +0800

    add ID to module

--- dmerge-kms/heap/src/lib.rs
+++ dmerge-kms/heap/src/lib.rs
@@ -8,6 +8,9 @@ use mitosis::linux_kernel_module;
 use dmerge::mitosis::Config;
 use crate::mitosis::syscalls::SysCallsService;
 
+use mitosis_macros::declare_module_param;
+
+declare_module_param!(mac_id, u64);
 #[allow(dead_code)]
 struct Module {
     service: SysCallsService<dmerge::DmergeSyscallHandler>,
@@ -15,12 +18,14 @@ struct Module {
 
 impl linux_kernel_module::KernelModule for Module {
     fn init() -> linux_kernel_module::KernelResult<Self> {
+        let id = mac_id::read();
+        log::info!("Dmerge kernel module assigned ID: {}", id);
         let mut config: Config = Default::default();
         config
             .set_num_nics_used(1)
             .set_rpc_threads(2)
             .set_init_dc_targets(12)
-            .set_machine_id(0 as usize);
+            .set_machine_id(id as usize);
         dmerge::startup::start_dmerge(&config);
 
         Ok(Self {
