a343ca3e7592467bd279db3f9a20d24d8d808a14
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-11-16 16:53:22 +0800

    update

--- samples/knative/python/ml-pipeline/demo.py
+++ samples/knative/python/ml-pipeline/demo.py
@@ -1,4 +1,9 @@
 import json
+from numpy import genfromtxt
+import numpy as np
+from numpy.linalg import eig
+import random
+import time
 
 test_data_file_path = 'dataset/Digits_Test.txt'
 train_data_file_path = 'dataset/Digits_Train.txt'
@@ -11,5 +16,31 @@ def dump_file_to_kv(path):
 
 
 content = dump_file_to_kv(train_data_file_path)
+train_data = genfromtxt(json.loads(content))
 
-print(len(content))
+print('finish download')
+start_time = int(round(time.time() * 1000))
+###########################
+train_labels = train_data[:, 0]
+
+A = train_data[:, 1:train_data.shape[1]]
+# calculate the mean of each column
+MA = np.mean(A.T, axis=1)
+# center columns by subtracting column means
+CA = A - MA
+# calculate covariance matrix of centered matrix
+VA = np.cov(CA.T)
+# eigendecomposition of covariance matrix
+values, vectors = eig(VA)
+# project data
+PA = vectors.T.dot(CA.T)
+
+np.save("/tmp/vectors_pca.txt", vectors)
+first_n_A = PA.T[:, 0:100].real
+train_labels = train_labels.reshape(train_labels.shape[0], 1)
+first_n_A_label = np.concatenate((train_labels, first_n_A), axis=1)
+np.savetxt("/tmp/Digits_Train_Transform.txt", first_n_A_label, delimiter="\t")
+###########################
+end_time = int(round(time.time() * 1000))
+
+print("run %d ms" % (end_time - start_time))
--- samples/knative/python/ml-pipeline/functions.py
+++ samples/knative/python/ml-pipeline/functions.py
@@ -1,5 +1,7 @@
-import redis_client
 from redis_client import *
+import numpy as np
+from numpy.linalg import eig
+import time
 
 test_data_file_path = 'dataset/Digits_Test.txt'
 train_data_file_path = 'dataset/Digits_Train.txt'
@@ -12,19 +14,41 @@ dump_file_to_kv(test_data_file_path)
 def splitter(meta):
     out_data = meta.copy()
 
-    content = read_file_from_kv(train_data_file_path)
-    redis_client.put(train_data_file_path, len(content))
+    train_data = np.genfromtxt(read_file_from_kv(train_data_file_path))
+    """
+    Start Execution
+    """
+    start_time = int(round(time.time() * 1000))
+    train_labels = train_data[:, 0]
+    A = train_data[:, 1:train_data.shape[1]]
+    # calculate the mean of each column
+    MA = np.mean(A.T, axis=1)
+    # center columns by subtracting column means
+    CA = A - MA
+    # calculate covariance matrix of centered matrix
+    VA = np.cov(CA.T)
+    # eigendecomposition of covariance matrix
+    values, vectors = eig(VA)
+    # project data
+    PA = vectors.T.dot(CA.T)
 
+    np.save("/tmp/vectors_pca.txt", vectors)
+    first_n_A = PA.T[:, 0:100].real
+    train_labels = train_labels.reshape(train_labels.shape[0], 1)
+    first_n_A_label = np.concatenate((train_labels, first_n_A), axis=1)
+    np.savetxt("/tmp/Digits_Train_Transform.txt", first_n_A_label, delimiter="\t")
+    """
+    End Execution
+    """
+    end_time = int(round(time.time() * 1000))
+
+    out_data['split/execution_ms'] = end_time - start_time
     out_data['key'] = train_data_file_path
     return out_data
 
 
 def trainer(meta):
     out_data = meta.copy()
-
-    data = redis_client.get(meta['key'])
-    out_data['len-of-content'] = data
-
     return out_data
 
 
