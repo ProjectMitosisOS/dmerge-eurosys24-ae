073d2914b2ab5ede6fb9e1532b36e62229eed2f5
Fangming Lu <lfm1012@sjtu.edu.cn>
2023-01-03 14:50:07 +0800

    update

--- samples/cpp/cpp/function.cc
+++ samples/cpp/cpp/function.cc
@@ -3,15 +3,15 @@
 
 static void test_allocator() {
     uint64_t base_addr = BASE;
-    uint64_t mem_sz = 1024 * 1024 * 512;
 
     base_addr += OFFSET;
     (*(int *) base_addr) = 4090;
     std::cout << std::dec << (uint64_t) (*(int *) base_addr) << "\n";
 
     int sd = sopen();
-    int heap_id = call_register(sd, (uint64_t) base_addr, 73);
+    int heap_id = call_register(sd, (uint64_t) base_addr);
     int res = *(int *) base_addr;
+//    std::cout << "heap id:" << heap_id << std::endl;
     std::cout << std::dec << "res:" << res << std::endl;
 }
 
