7c47694d0a8901ff8b3f12c91ea05aa61628b38b
Fangming Lu <lfm1012@sjtu.edu.cn>
2023-01-04 10:14:31 +0800

    update

--- samples/python/function.py
+++ samples/python/function.py
@@ -4,12 +4,11 @@ from bindings import *
 
 addr = 0x4ffff5a00000
 OFFSET = 1024 * 4
-res = ccreate_heap(start_addr=addr, mem_sz=1024 * 1024)
+# res = ccreate_heap(start_addr=addr, mem_sz=1024 * 1024)
 my_write_ptr(addr=addr + OFFSET, val=124125)
 print(my_read_ptr(addr + OFFSET))
 
 sd = sopen()
 hint = call_register(sd=sd, peak_addr=addr, hint=1)
-
 while True:
     time.sleep(1)
