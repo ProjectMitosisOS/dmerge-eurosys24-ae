bd4b299b67d513b6b1a6b821fe8197c209ae5c77
Fangming Lu <lfm1012@sjtu.edu.cn>
2023-02-21 15:37:02 +0800

    update

--- samples/python/function.py
+++ samples/python/function.py
@@ -1,12 +1,9 @@
 import time
-
 from bindings import *
 
+obj = range(3)
 addr = 0x4ffff5a00000
-
 # obj = 'hello world'
-obj = [3, 4]
-obj.append('hello')
 print(hex(id(obj)), id(obj))
 print(id_deref(id(obj)))
 
