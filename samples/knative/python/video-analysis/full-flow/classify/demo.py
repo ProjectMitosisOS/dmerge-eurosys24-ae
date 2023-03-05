25f0d6f9db1cf0b55ba3065879758cac7097ae7d
CaribouW <lfm1012@sjtu.edu.cn>
2023-03-05 12:11:54 +0800

    update

--- samples/knative/python/video-analysis/full-flow/classify/demo.py
+++ samples/knative/python/video-analysis/full-flow/classify/demo.py
@@ -3,52 +3,55 @@ from imageai.Detection import ObjectDetection
 from multiprocessing import Process, Manager
 import multiprocessing
 import time
+from minio import Minio
 from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageFile
 import zipfile
 import os
 
-# pip install -i https://pypi.python.org/simple tensorflow==2.4.0
-
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+# s3_client = Minio(
+#     endpoint='127.0.0.1:9000',
+#     secure=False,
+#     access_key='wSgN80XAPlP5tLgD', secret_key='z9YsQtR2QENolHcM4mrqWLPSYiJBoa7W')
+bucket_name = 'video-analysis'
 
 
 def delete_tmp():
-    for root, dirs, files in os.walk("./tmp/", topdown=False):
+    for root, dirs, files in os.walk("/tmp/", topdown=False):
         for name in files:
             os.remove(os.path.join(root, name))
         for name in dirs:
             os.rmdir(os.path.join(root, name))
 
 
-def detect_object(index, index2, image):
+def detect_object(index, index2, image, src_video, milli_1, milli_2, detect_prob):
     detector = ObjectDetection()
-    model_path = "./models/yolo-tiny.h5"
+    model_path = "/Users/lufangming/Documents/repos/dmerge/samples/knative/python/video-analysis/full-flow/classify/models/yolo-tiny.h5"
 
     start_time = int(round(time.time() * 1000))
 
-    bucket_name = "XXXXXXXXXXXXXXXXX"
-
-    worker_dir = "./tmp/" + "Worker_" + str(index)
-
+    worker_dir = "/tmp/" + "Worker_" + str(index)
     if not os.path.exists(worker_dir):
         os.mkdir(worker_dir)
 
-    filename = "/image_" + str(index) + ".jpg"
+    filename = worker_dir + "/image_" + str(index) + ".jpg"
+    key = image
+    # s3_client.fget_object(bucket_name, key, filename)
     print("Download duration: " + str(time.time() * 1000 - start_time))
     # input_path = "~/images/input_fast.jpg"
     # if(index == 29):
     #   input_path = "~/images/input_slow.jpg"
 
-    output_path = "./images/output_" + str(index) + ".jpg"
+    output_path = "images/output_" + str(index) + ".jpg"
     detector.setModelTypeAsTinyYOLOv3()
 
     detector.setModelPath(model_path)
     detector.loadModel()
 
     start_time = time.time() * 1000
-    # TODO: File path
+
     detection = detector.detectObjectsFromImage(input_image=filename, output_image_path=output_path,
-                                                minimum_percentage_probability=2)
+                                                minimum_percentage_probability=detect_prob)
 
     for box in range(len(detection)):
         print(detection[box])
@@ -69,11 +72,16 @@ def detect_object(index, index2, image):
             ths[t].start()
         for t in range(threads):
             ths[t].join()
-    zipFileName = "detected_images_" + str(index) + ".zip"
-    myzip = zipfile.ZipFile("./tmp/" + zipFileName, 'w', zipfile.ZIP_DEFLATED)
+    millis_3 = int(round(time.time() * 1000))
+    zipFileName = "detected_images_" + str(src_video) + "_" + str(index) + "_" + str(milli_1[0]) + "_" + str(
+        milli_2[0]) + "_" + str(millis_3) + ".zip"
+    myzip = zipfile.ZipFile("/tmp/" + zipFileName, 'w', zipfile.ZIP_DEFLATED)
 
     for f in os.listdir(worker_dir):
         myzip.write(worker_dir + "/" + f)
+
+    # s3_client.fput_object(bucket_name, "Detected_Objects/" + zipFileName, "/tmp/" + zipFileName)
+    print("file uploaded " + zipFileName)
 
 
 def crop_and_sharpen(original_image, t, detection, start_index, end_index, worker_dir):
@@ -90,31 +98,44 @@ def crop_and_sharpen(original_image, t, detection, start_index, end_index, worke
     print(len(detection))
 
 
-def handler(event, context):
+def handler(event):
     start_time = int(round(time.time() * 1000))
 
-    # config = TransferConfig(use_threads=False)
     print(event)
-    bucket_name = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    bucketName = bucket_name
 
     list_of_chunks = event['values']
-    list_of_images = event['images']
+    # list_of_images = event['images']
 
     print(list_of_chunks[0])
-    # src_video = event['source_id']
-    # millis_list1 = event['millis1']
-    # millis_list2 = event['millis2']
+    src_video = event['source_id']
+    millis_list1 = event['millis1']
+    millis_list2 = event['millis2']
+    detect_prob = 80  # from 0 to 100
 
     ths = []
     num_workers = len(list_of_chunks)
     for w in range(num_workers):
-        ths.append(Process(target=detect_object, args=(list_of_chunks[w], list_of_chunks[w], list_of_images[w])))
+        key = "Video_Frames_Step/frame_" + str(src_video) + "_" + str(list_of_chunks[w]) + "_" + str(
+                millis_list1[w]) + "_" + str(millis_list2[w]) + ".jpg"
+        detect_object(list_of_chunks[w], list_of_chunks[w], key, src_video, millis_list1, millis_list2, detect_prob)
+        # key = "Video_Frames_Step/frame_" + str(src_video) + "_" + str(list_of_chunks[w]) + "_" + str(
+        #     millis_list1[w]) + "_" + str(millis_list2[w]) + ".jpg"
+        # ths.append(Process(target=detect_object, args=(
+        #     list_of_chunks[w], list_of_chunks[w], key, src_video, millis_list1, millis_list2, detect_prob)))
 
-    for t in range(num_workers):
-        ths[t].start()
-    for t in range(num_workers):
-        ths[t].join()
+    # for t in range(num_workers):
+    #     ths[t].start()
+    # for t in range(num_workers):
+    #     ths[t].join()
 
     end_time = time.time() * 1000
-    print("duration: " + str(end_time - start_time))
-    delete_tmp()
+    diff_time = str(end_time - start_time)
+    print("duration: " + diff_time)
+    return {'duration': diff_time, 'values': str(event['values'])}
+
+    # delete_tmp()
+
+
+if __name__ == '__main__':
+    pass
--- samples/knative/python/video-analysis/full-flow/main.py
+++ samples/knative/python/video-analysis/full-flow/main.py
@@ -1,18 +1,18 @@
 import split
 import extract
+import classify
 
 if __name__ == '__main__':
     # Step1: Split
     result = split.lambda_handler(src_video=3, partition_num=3)
     events = result["detail"]["indeces"]
-    return_list = []
 
+    res_list = []
     # Step2: Extract
     for event in events:  # Should run in parallel
         extract_result = extract.lambda_handler(event)
-        return_list.append(extract_result)
-
-    print(len(return_list))
-
-    # Step3: Classify
-
+        print(extract_result)
+        # Step3: Classify
+        classify_res = classify.handler(extract_result)
+        print(classify_res)
+        break
