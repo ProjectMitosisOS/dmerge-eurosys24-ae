2b2f1d64714086a28b8359551f8fbce1a0c21807
CaribouW <lfm1012@sjtu.edu.cn>
2023-03-08 15:07:35 +0800

    finish jemalloc replace

--- exp/p2p/functions.py
+++ exp/p2p/functions.py
@@ -10,7 +10,6 @@ s3_client = Minio(
     secure=False,
     access_key='ACCESS_KEY', secret_key='SECRET_KEY')
 bucket_name = 'p2p'
-
 if not s3_client.bucket_exists(bucket_name):
     s3_client.make_bucket(bucket_name)
 
@@ -35,22 +34,44 @@ networkTimeStr = 'network_time'
 
 
 def splitter(meta):
+    data_path = 'data/digits.txt'
+    s3_client.fput_object(bucket_name, 'digits', data_path)
     current_app.logger.info(f'splitter, {meta}')
     return {
         'status': 200,
+        's3_obj_key': 'digits',
         'wf_id': str(uuid.uuid4())
     }
 
 
-def producer(meta):
-    current_app.logger.info(f'producer, {meta}')
-    out_meta = meta
+import numpy as np
+from numpy.linalg import eig
 
+global_obj = {}
+
+
+def producer(meta):
+    s3_obj = meta['s3_obj_key']
+    local_data_path = '/tmp/digits.txt'
+    worker_id = int(os.environ.get('ID', 0))
+
+    current_app.logger.info(f'Get from s3 with key: {s3_obj}')
+    s3_client.fget_object(bucket_name, s3_obj, local_data_path)  # download all
+    data = np.genfromtxt(local_data_path, delimiter='\t')
+    global_obj['data'] = data
+    out_meta = dict(meta)
+
+    out_meta.pop('s3_obj_key')
+    out_meta['obj_hash'] = {
+        'data': id(global_obj['data'])
+    }
     return out_meta
 
 
 def consumer(metas):
-    current_app.logger.info(f'consumer, {metas}')
+    target_id = metas[-1]['obj_hash']['data']
+    current_app.logger.info(f'consumer with meta: {metas}. Target id {hex(target_id)}')
+
     return metas[-1] if len(metas) > 0 else 0
 
 
@@ -63,7 +84,7 @@ def sink(metas):
 
 def default_handler(meta):
     current_app.logger.info(f'not a default path for type')
-    return {}
+    return meta
 
 
 if __name__ == '__main__':
