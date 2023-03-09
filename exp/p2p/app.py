5fd9f5798007b0e7b5cd86fa4491a48d10e51472
CaribouW <lfm1012@sjtu.edu.cn>
2023-03-09 16:42:12 +0800

    update: fix bug for

--- exp/p2p/app.py
+++ exp/p2p/app.py
@@ -117,4 +117,4 @@ def handle_ce(event: CloudEvent):
 
 if __name__ == '__main__':
     port = 8080
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
--- samples/knative/install/uninstall_serving.sh
+++ samples/knative/install/uninstall_serving.sh
@@ -1,12 +1,10 @@
-kubectl delete -f https://github.com/knative/serving/releases/download/knative-v1.7.2/serving-hpa.yaml
+kubectl delete -f yaml/serving-hpa.yaml
 
-kubectl delete -f https://github.com/knative/net-contour/releases/download/knative-v1.7.0/net-contour.yaml
+kubectl delete -f yaml/kourier.yaml
 
-kubectl delete -f https://github.com/knative/net-contour/releases/download/knative-v1.7.0/contour.yaml
+kubectl delete -f yaml/serving-core.yaml
 
-kubectl delete -f https://storage.googleapis.com/knative-nightly/serving/latest/serving-core.yaml
-
-kubectl delete -f https://storage.googleapis.com/knative-nightly/serving/latest/serving-crds.yaml
+kubectl delete -f yaml/serving-crds.yaml
 
 
 
