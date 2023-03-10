30673f4b623e4a9e940c44b277b43ae45571066a
CaribouW <lfm1012@sjtu.edu.cn>
2023-03-10 10:24:28 +0800

    pca

--- exp/ml-pipeline/functions.py
+++ exp/ml-pipeline/functions.py
@@ -42,35 +42,90 @@ def fill_gid(gid):
 
 
 def source(meta):
+    data_path = 'dataset/Digits_Train.txt'
+    s3_object_key = 'digits'
+    s3_client.fput_object(bucket_name, s3_object_key, data_path)
+    wf_start_tick = cur_tick_ms()
     return {
         'status': 200,
-        # 's3_obj_key': 'digits',
+        's3_obj_key': s3_object_key,
         'wf_id': str(uuid.uuid4()),
         'features': {
             'protocol': os.environ.get('PROTOCOL', 'S3')  # value of DMERGE / S3 / P2P --- Default as S3
         },
         'profile': {
-            'leave_tick': cur_tick_ms()
+            'leave_tick': wf_start_tick,
+            'wf_start_tick': wf_start_tick
         },
     }
 
 
 def pca(meta):
+    local_data_path = '/tmp/digits.txt'
+
+    def execute_body(train_data):
+        train_labels = train_data[:, 0]
+        A = train_data[:, 1:train_data.shape[1]]
+        # calculate the mean of each column
+        MA = np.mean(A.T, axis=1)
+        # center columns by subtracting column means
+        CA = A - MA
+        # calculate covariance matrix of centered matrix
+        VA = np.cov(CA.T)
+        # eigendecomposition of covariance matrix
+        values, vectors = eig(VA)
+        # project data
+        PA = vectors.T.dot(CA.T)
+        train_labels = train_labels.reshape(train_labels.shape[0], 1)
+        first_n_A = PA.T[:, 0:100].real
+        first_n_A_label = np.concatenate((train_labels, first_n_A), axis=1)
+        return vectors, first_n_A_label
+
     def pca_s3(_meta, data):
         out_meta = dict(_meta)
+        # 1. Execute
+        execute_start_time = cur_tick_ms()
+        vectors, first_n_A_label = execute_body(data)
+        execute_end_time = cur_tick_ms()
+        # 2. Deserialize
+        sd_start_time = cur_tick_ms()
+        np.save("/tmp/vectors_pca.txt", vectors)
+        np.savetxt("/tmp/Digits_Train_Transform.txt", first_n_A_label, delimiter="\t")
+        sd_end_time = cur_tick_ms()
+        s3_client.fput_object(bucket_name, 'ML_Pipeline/vectors_pca.txt', '/tmp/vectors_pca.txt.npy')
+        # 3. dump to s3
+        external_resource_start_time = cur_tick_ms()
+        s3_client.fput_object(bucket_name, 'ML_Pipeline/train_pca_transform_2.txt',
+                              '/tmp/Digits_Train_Transform.txt')
+        external_resource_end_time = cur_tick_ms()
+
+        out_meta['s3_obj_key'] = 'ML_Pipeline/train_pca_transform_2.txt'
+        out_meta['profile'].update({
+            'pca': {
+                'execute_time': execute_end_time - execute_start_time,
+                'sd_time': sd_end_time - sd_start_time,
+                's3_time': external_resource_end_time - external_resource_start_time
+            },
+            'leave_tick': cur_tick_ms()
+        })
         current_app.logger.info(f"Inner pca s3 with meta: {out_meta}")
         return out_meta
 
+    s3_obj = meta['s3_obj_key']
+    s3_client.fget_object(bucket_name, s3_obj, local_data_path)  # download all
+    train_data = np.genfromtxt(local_data_path, delimiter='\t')
     pca_dispatcher = {
         'S3': pca_s3
     }
     dispatch_key = meta['features']['protocol']
-    return pca_dispatcher[dispatch_key](meta, None)
+    return pca_dispatcher[dispatch_key](meta, train_data)
 
 
 def trainer(metas):
     def trainer_s3(_metas, data):
         out_meta = _metas[-1]
+        for event in _metas:
+            pass
         current_app.logger.info(f"Inner Trainer s3 with meta: {out_meta}")
         return out_meta
 
@@ -85,6 +140,13 @@ def trainer(metas):
 def combinemodels(metas):
     def combine_models_s3(_metas, data):
         out_meta = _metas[-1]
+        wf_end_tick = cur_tick_ms()
+        for event in _metas:
+            pass
+        wf_start_tick = out_meta['profile']['wf_start_tick']
+        out_meta['profile'].update({
+            'wf_e2e_time': wf_end_tick - wf_start_tick
+        })
         current_app.logger.info(f"Inner combine_models s3 with meta: {out_meta}")
         return out_meta
 
