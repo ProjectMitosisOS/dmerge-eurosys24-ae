726302357d4f7247dc5dfa344af27533c56ce0ce
CaribouW <lfm1012@sjtu.edu.cn>
2023-03-15 16:35:15 +0800

    update

--- exp/finra/full-flow.py
+++ exp/finra/full-flow.py
@@ -1,3 +1,4 @@
+import pickle
 import socket
 import time
 
@@ -5,6 +6,15 @@ import numpy as np
 import pandas as pd
 from util import *
 import os
+from minio import Minio
+
+s3_client = Minio(
+    endpoint='127.0.0.1:9000',
+    secure=False,
+    access_key='wSgN80XAPlP5tLgD', secret_key='z9YsQtR2QENolHcM4mrqWLPSYiJBoa7W')
+bucketName = 'finra'
+if not s3_client.bucket_exists(bucketName):
+    s3_client.make_bucket(bucketName)
 
 
 def cur_tick_ms():
@@ -24,24 +34,46 @@ def public_data(event):
     tickers = tickers_for_portfolio_types[portfolioType]
 
     prices = {}
+    tick = cur_tick_ms()
     whole_set = pd.read_csv("yfinance.csv")
+
     for ticker in tickers:
         # Get last closing price
         tickTime = 1000 * time.time()
-        data = pd.read_csv("yfinance.csv")
         externalServicesTime += 1000 * time.time() - tickTime
         prices[ticker] = whole_set['Close'].unique()[0]
 
-    response = {'statusCode': 200,
-                'body': {'marketData': prices, 'whole_set': whole_set}}
+    out_meta = {'statusCode': 200,
+                'body': {'marketData': prices}}
+    s3_obj_key = 'whole_data'
+    local_file_name = 'whole_data.npy'
+    d = np.array(whole_set)
+    execute_time = cur_tick_ms() - tick
 
-    return response
+    tick = cur_tick_ms()
+    np.save(local_file_name, d)
+    sd_time = cur_tick_ms() - tick
+
+    tick = cur_tick_ms()
+    s3_client.fput_object(bucketName, s3_obj_key, local_file_name)
+    s3_time = cur_tick_ms() - tick
+
+    out_meta['s3_obj_key'] = s3_obj_key
+    out_meta['profile'] = {
+        'public_data': {
+            'execute_time': execute_time,
+            'sd_time': sd_time,
+            's3_time': s3_time
+        }
+    }
+
+    return out_meta
 
 
 # parallel step2: private data
 def private_data(event):
     portfolio = event['body']['portfolio']
-
+    tick = cur_tick_ms()
     data = portfolios[portfolio]
 
     valid = True
@@ -52,8 +84,16 @@ def private_data(event):
         if not (side == 1 or side == 2 or side == 8):
             valid = False
             break
-    response = {'statusCode': 200, 'body': {'valid': valid, 'portfolio': portfolio}}
-    return response
+    execute_time = cur_tick_ms() - tick
+    out_meta = {
+        'statusCode': 200,
+        'body': {'valid': valid, 'portfolio': portfolio},
+        'profile': {
+            'private_data': {
+                'execute_time': execute_time
+            }
+        }}
+    return out_meta
 
 
 # parallel steps: bargain balance
@@ -82,29 +122,54 @@ def bargin_balance(events):
     marketData = {}
     whole_set = {}
     validFormat = True
+    finance_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends',
+                       'Stock Splits']
+    execute_time = 0
+    s3_time = 0
+    sd_time = 0
 
     for event in events:
         body = event['body']
         if 'marketData' in body:
+            tick = cur_tick_ms()
             marketData = body['marketData']
-            whole_set = body['whole_set']
+            # Get whole set
+            s3_obj_key = event['s3_obj_key']
+            local_path = 'tmp.npy'
+            s3_client.fget_object(bucketName, s3_obj_key, local_path)
+            s3_time += cur_tick_ms() - tick
+
+            tick = cur_tick_ms()
+            data = np.load(local_path, allow_pickle=True)
+            whole_set = pd.DataFrame(data, columns=finance_columns)
+            sd_time += cur_tick_ms() - tick
         elif 'valid' in body:
             portfolio = event['body']['portfolio']
             validFormat = validFormat and body['valid']
 
+    tick = cur_tick_ms()
     portfolioData = portfolios[portfolio]
     marginSatisfied = checkMarginBalance(portfolioData, marketData, portfolio)
-    finance_columns = ['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits']
-    avg_data = [np.average(whole_set[key]) for key in finance_columns]
-    sum_data = [np.sum(whole_set[key]) for key in finance_columns]
-    std_data = [np.std(whole_set[key]) for key in finance_columns]
-    whole_data = [avg_data, sum_data, std_data]
-    response = {'statusCode': 200,
-                'body': {'validFormat': validFormat,
-                         'marginSatisfied': marginSatisfied,
-                         'whole_data': whole_data
-                         }}
-    return response
+    avg_data = [whole_set[key] for key in finance_columns]
+    sum_data = [whole_set[key] for key in finance_columns]
+    std_data = [whole_set[key] for key in finance_columns]
+    whole_data = [avg_data[0], sum_data[0], std_data[0]]
+    execute_time = cur_tick_ms() - tick
+    out_meta = {
+        'statusCode': 200,
+        'body': {
+            'validFormat': validFormat,
+            'marginSatisfied': marginSatisfied,
+            'whole_data': whole_data
+        },
+        'profile': {
+            'bargain_balance': {
+                'execute_time': execute_time,
+                's3_time': s3_time,
+                'sd_time': sd_time
+            }
+        }}
+    return out_meta
 
 
 if __name__ == '__main__':
@@ -113,4 +178,6 @@ if __name__ == '__main__':
     events = [public_data(req), private_data(req)]
     res = bargin_balance(events)
     print(f'time passed {cur_tick_ms() - tick} ms')
-    print(res)
+    print(events[0]['profile'])
+    print(events[1]['profile'])
+    print(res['profile'])
